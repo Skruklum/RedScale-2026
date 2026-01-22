88c2b5bad2e399d1dcfdce6c2e77c6a65aac98ac
Fazl Nazheef <fazlnazheef@gmail.com>
2026-01-22 14:51:26 +0700

    kA test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.6713335424967015;
         public double kV = 0.00018576033406672958;
-        public double kA = 0;
+        public double kA = 0.0000001 ;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
