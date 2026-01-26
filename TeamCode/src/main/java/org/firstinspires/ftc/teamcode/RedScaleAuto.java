0155064854460cbcf276b129b6b70979a89544a2
Fazl Nazheef <fazlnazheef@gmail.com>
2026-01-26 17:17:51 +0700

    autonomous punya elu gi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedScaleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedScaleAuto.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
-public class RedScaleAuto {
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "RR_Auto_Converted", group = "Autonomous")
+public class RedScaleAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Initialize your MecanumDrive (this contains your 2-dead wheel localizer)
+        // Make sure the starting Pose matches your MeepMeep code exactly
+        Pose2d initialPose = new Pose2d(-70, 29, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Build and execute the action
+        Actions.runBlocking(
+                drive.actionBuilder(initialPose)
+                        .strafeTo(new Vector2d(-13, 29))
+                        .strafeTo(new Vector2d(-16, 0))
+                        .strafeTo(new Vector2d(13, 29))
+                        .strafeTo(new Vector2d(-16, 0))
+                        .strafeTo(new Vector2d(37, 29))
+                        .strafeTo(new Vector2d(-16, 0))
+                        .strafeTo(new Vector2d(39, 30))
+                        .build()
+        );
+    }
 }
