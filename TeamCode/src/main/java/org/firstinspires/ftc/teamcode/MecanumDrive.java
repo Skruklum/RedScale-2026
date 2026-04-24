e4e1c1ae109dfd53a8a5eb7ff9c9d195b1e68650
Fazl Nazheef <fazlnazheef@gmail.com>
2026-04-24 11:32:57 +0700

    Yoooo Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,12 +64,12 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.00090212;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 583.2644841817425;
+        public double lateralInPerTick = 0.000544032742630305;
+        public double trackWidthTicks = 13782.905168848918;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 3.072211302734758;
+        public double kV = 0.00007690693046231067;
         public double kA = 0;
 
         // path profile parameters (in inches)
@@ -239,7 +239,6 @@ public final class MecanumDrive {
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
@@ -28,8 +28,8 @@ import org.firstinspires.ftc.teamcode.messages.TwoDeadWheelInputsMessage;
 @Config
 public final class TwoDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = 0; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = 0; // x position of the perpendicular encoder (in tick units)
+        public double parYTicks = -5952.674327952829; // y position of the parallel encoder (in tick units)
+        public double perpXTicks = -1570.0433393634835; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
